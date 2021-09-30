edca25c7d889f12e76ed331991dee751c45517d0
Emma Egstad <emma@egstad.com>
2021-09-30 14:17:35 -0700

    passing test for sub function

--- test/example.test.js
+++ test/example.test.js
@@ -1,25 +1,10 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
-import { add_two } from '../calculations.js';
 import { add } from '../calculations.js';
+import { sub } from '../calculations.js';
 
 const test = QUnit.test;
 
-test('test add_two function', (expect) => {
-  //Arrange
-  // Set up your arguments and expectations
-  const expected = 4;
-
-  //Act 
-  // Call the function you're testing and set the result to a const
-  const actual = add_two(2);
-
-  //Expect
-  // Make assertions about what is expected versus the actual result
-  expect.equal(actual, expected);
-});
-
-test('add(3, 2) function', (expect) => {
+test('test add function', (expect) => {
   //Arrange
   const input1 = 3;
   const input2 = 2;
@@ -32,15 +17,16 @@ test('add(3, 2) function', (expect) => {
   expect.equal(actual, expected);
 });
 
-test('add(4, 5) function', (expect) => {
+test('test sub function', (expect) => {
   //Arrange
-  const input1 = 4;
-  const input2 = 5;
-  const expected = 9;
+  const input1 = 100;
+  const input2 = 40;
+  const expected = 60;
 
   //Act
-  const actual = add(input1, input2);
+  const actual = sub(input1, input2);
 
   //Assert
   expect.equal(actual, expected);
+
 });
